f6c994a3afca45aceca25a6eb85410e4c1ea492c
panda-with-issues <ryoda94@gmail.com>
2019-11-10 10:15:42 +0100

    solve problem 2

--- Esercizi/tiles.rkt
+++ Esercizi/tiles.rkt
@@ -5,8 +5,8 @@
 ; We have a wall of dimension 12 * 1 and two tiles, the first with dimension 1 * 1 and the second woth dimension 1 * 2
 ; How many ways are there to tile the stripe?
 
-(define tiling-a
-  (lambda (length)
+(define tiling-a    ; val: integer non-negative number
+  (lambda (length)  ; same as val
     ; base cases
     (cond ((= length 1)
            1
@@ -29,4 +29,30 @@
 (tiling-a 12) ; 233
 
 ; Problem 2:
-; 
+; Now the wall is 11 long and we have two tiles with dimension 1 * 1, one red and the other blue.
+; We don't want any red tiles to be next each other. How many ways can we tile now?
+
+(define tiling-b    ; integer non-negative number
+  (lambda (length)  ; same as vale
+    ; base cases
+    (cond ((= length 1)
+           ; i can use both a blue or a red
+           2
+           )
+          ((= length 2)
+           ; i can use 2 blue, or one blue and one red in any order
+           3
+           )
+          (else
+           ; recursive step
+           (+
+            (tiling-b (- length 1)) ; i'm using a blue tile
+            (tiling-b (- length 2)) ; i'm using a red tile, so i'm forced to use a blue next
+            )
+           )
+          )
+    ))
+
+;; test
+
+(tiling-b 11) ; 233
