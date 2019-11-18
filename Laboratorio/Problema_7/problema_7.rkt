5a6e05212532f597e1dbd905c6d75bae87cd479d
panda-with-issues <ryoda94@gmail.com>
2019-11-18 09:25:02 +0100

    implement position, a procedure to return an element's index

--- Laboratorio/Problema_7/problema_7.rkt
+++ Laboratorio/Problema_7/problema_7.rkt
@@ -1,8 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname problema_7) (read-case-sensitive #t) (teachpacks ((lib "drawings.ss" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "drawings.ss" "installed-teachpacks")) #f)))
-(define belong?  ; val: bool, check whether an item is in a given list
-  (lambda (i lst)  ; i: integer, lst: list of integers repeated no more than once
+(define belong?    ; val: bool, check whether an item is in a given list
+  (lambda (i lst)  ; i: integer, lst: list of sorted integers
     ; base cases
     (cond ((null? lst)
            #f
@@ -21,3 +21,13 @@
 (belong? 18 '())             ; #false
 (belong? 18 '(5 7 10 18 23)) ; #true
 (belong? 18 '(5 7 10 12 23)) ; #false
+
+(define position   ; val: non-negative integer, index of an item in a given list
+  (lambda (i lst)  ; i: integer, lst: sorted list of integer in which each number is repeated no more than once
+    ; base case
+    (if (= (car lst) i)
+        0
+        ; recursive step
+        (add1 (position i (cdr lst)))
+        )
+    ))
