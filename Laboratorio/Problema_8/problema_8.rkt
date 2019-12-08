104ee8bd6b6fb41c70b974e0536a533f811ea89d
panda-with-issues <ryoda94@gmail.com>
2019-12-08 18:38:52 +0100

    refactor code with curses. Not working atm

--- Laboratorio/Problema_8/problema_8.rkt
+++ Laboratorio/Problema_8/problema_8.rkt
@@ -21,111 +21,48 @@
 
 ;; task 1
 
-(define legal-move
-  (lambda (n k s d t)
-    (let ((half (expt 2 (sub1 n)))
-          (r (remainder k 3))
-          )
-      (if (<= k half)
-          (cond
-            ((= r 0)
-             (list d t)
-             )
-            ((= r 1)
-             (list s d)
-             )
-            ((= r 2)
-             (list s t)
-             )
-            )
-          (cond
-            ((= r 0)
-             (list t d)
-             )
-            ((= r 1)
-             (list s d)
-             )
-            ((= r 2)
-             (list t s)
-             )
-            )
-          )
-      )
-    ))
-
-(define moves-list
-  (lambda (n k)
-    (if (zero? k)
-        null
-        (append (moves-list n (sub1 k)) (list (legal-move n k 1 2 3)) )
-        )
-    ))
-
-(define move-disks
-  (lambda (moves s d t disks)
-    (if (null? moves)
-        disks
-    (let ((move (car moves))
-          (s* (car move))
-          (d* (cdr move))
-          (s-num (car disks))
-          (d-num (car (cdr disks)))
-          (t-num (cdr (cdr disks)))
-          )
+(define locate-bigger
+  (lambda (n k s d t s-num d-num t-num)
+    (let ((half-- (expt 2 (- n 2)))
+          (offset (expt 2 (sub1 n))))
       (cond
-        ((= s* s)
-         (cond
-           ((= d* d)  ; s -> d
-            (move-disks (cdr moves) s d t (list (sub1 s-num) (add1 d-num) t-num))
-            )
-           ((= d* t)  ; s -> t
-            (move-disks (cdr moves) s d t (list (sub1 s-num) d-num (add1 t-num)))
-            )
-           )
+        ; base case
+        ((= n 1)
+         (if (<= k 0)
+             (list (list s (add1 s-num)) (list d d-num) (list t t-num))
+             (list (list s s-num) (list d (add1 d-num)) (list t t-num))
+             )
          )
-        ((= s* d)
-         (cond
-           ((= d* s)  ; d -> s
-            (move-disks (cdr moves) s d t (list (add1 s-num) (sub1 d-num) t-num))
-            )
-           ((= d* t)  ; d -> t
-            (move-disks (cdr moves) s d t (list s-num (sub1 d-num) (add1 t-num)))
-            )
-           )
+        ; k is smaller than n-1 tower's middle move
+        ((<= k half--)
+         (locate-bigger (sub1 n) k s t d (add1 s-num) t-num d-num)
          )
-        ((= s* t)
-         (cond
-           ((= d* s)  ; t -> s
-            (move-disks (cdr moves) s d t (list (add1 s-num) d-num (add1 t-num)))
-            )
-           ((= d* d)  ; t -> d
-            (move-disks (cdr moves) s d t (list s-num (add1 d-num) (sub1 t-num)))
-            )
-           )
+        ; k is smaller than middle move
+        ((< k (expt 2 (sub1 n)))
+         (locate-bigger (sub1 n) (quotient k 2) s t d (add1 s-num) t-num d-num)
+         )
+        (else
+         (locate-bigger (sub1 n) (- k offset) t d s t-num (add1 d-num) s-num)
          )
         )
       )
-      )
     ))
 
-(define disk-position
-  (lambda (n k s d t disks)
-   (if (null? (moves-list n k))
-       disks
-       (let (move))
-       )
+(define hanoi-disks
+  (lambda (n k)
+    (locate-bigger n k 1 2 3 0 0 0)
     ))
 
 ;; test
 
-;(hanoi-disks 3 0)      ;'((1 3) (3 0) (2 0))
-;(hanoi-disks 3 1)      ;'((3 0) (2 1) (1 2))
-;(hanoi-disks 3 2)      ;'((2 1) (1 1) (3 1))
-;(hanoi-disks 3 3)      ;'((1 1) (3 2) (2 0))
-;(hanoi-disks 3 4)      ;'((3 2) (2 1) (1 0))
-;(hanoi-disks 3 5)      ;'((2 1) (1 1) (3 1))
-;(hanoi-disks 3 6)      ;'((1 1) (3 0) (2 2))
-;(hanoi-disks 3 7)      ;'((3 0) (2 3) (1 0))
+(hanoi-disks 3 0)      ;'((1 3) (3 0) (2 0)) ok
+(hanoi-disks 3 1)      ;'((3 0) (2 1) (1 2)) ok
+(hanoi-disks 3 2)      ;'((2 1) (1 1) (3 1)) ok
+(hanoi-disks 3 3)      ;'((1 1) (3 2) (2 0)) non ok
+(hanoi-disks 3 4)      ;'((3 2) (2 1) (1 0)) non ok
+(hanoi-disks 3 5)      ;'((2 1) (1 1) (3 1)) non ok
+(hanoi-disks 3 6)      ;'((1 1) (3 0) (2 2)) ok
+(hanoi-disks 3 7)      ;'((3 0) (2 3) (1 0)) ok
 ;(hanoi-disks 5 13)     ;'((3 2) (2 1) (1 2))
 ;(hanoi-disks 15 19705) ;'((3 4) (2 9) (1 2))
 ;(hanoi-disks 15 32767) ;'((3 0) (2 15) (1 0)
