5c2624625ff3f87fc9061d8ca93b18e0b80f7a44
panda-with-issues <ryoda94@gmail.com>
2019-12-09 13:20:36 +0100

    solve task 3, add some comments

--- Laboratorio/Esercitazione_di_esame_1/esercitazione_1.rkt
+++ Laboratorio/Esercitazione_di_esame_1/esercitazione_1.rkt
@@ -18,7 +18,7 @@
 
 ;; test
 
-(match "astrazione" "estremi")
+(match "astrazione" "estremi") ; "*str**i"
 
 ;; task 2
 
@@ -35,11 +35,49 @@
     (let ((digits (string-length num)))
       (if (= digits 0)
           "1"
-          (let (dgt (string-ref num (sub1 digits)))
+          (let ((dgt (string-ref num (sub1 digits))))
             (if (char=? dgt (last-digit base))
                 (string-append (increment (substring num 0 (sub1 digits)) base)
                                "0")
-                (string-append (substring num 0 (- digits 1)) (next-digit (dgt)))
+                (string-append (substring num 0 (- digits 1)) (next-digit dgt))
                 ))
           ))))
+
+;; test
+
+(increment "1011" 2) ; "1100"
+
+;; task 3
+
+(define lcs      ; valore: lista di terne
+  (lambda (u v)  ; u, v: stringhe
+    (lcs-rec 1 u 1 v)
+    ))
+
+(define lcs-rec
+  (lambda (i u j v)
+    (cond ((or (string=? u "") (string=? v ""))
+           null
+           )
+          ((char=? (string-ref u 0) (string-ref v 0))
+           (cons (list i j (substring u 0 1))
+                 (lcs-rec (+ i 1) (substring u 1) (+ j 1) (substring v 1)) ))
+          (else
+           (better (lcs-rec (add1 i) (substring u 1) j v)
+                   (lcs-rec i u (add1 j) (substring v 1))))
+           )))
+
+(define better
+  (lambda (x y)
+    (if (< (length x) (length y)) y x)
+        ))
+
+;; tests
+
+(lcs "pino" "pino")  ; ((1 1 "p") (2 2 "i") (3 3 "n") (4 4 "o"))
+(lcs "pelo" "peso")  ; ((1 1 "p") (2 2 "e") (4 4 "o"))
+(lcs "ala" "palato") ; ((1 2 "a") (2 3 "l") (3 4 "a"))
+(lcs "arto" "atrio") ; ((1 1 "a") (3 2 "t") (4 5 "o"))
+
+
               
