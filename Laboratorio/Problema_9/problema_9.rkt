413f5457a1380f0c686eb9aedf68365d2d99ea51
panda-with-issues <ryoda94@gmail.com>
2020-03-15 16:28:57 +0100

    solve task 2

--- Laboratorio/Problema_9/problema_9.rkt
+++ Laboratorio/Problema_9/problema_9.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname problema_9) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Task 1
+ ;; Task 1
 
 (define alphabet "ABCDEFGHILMNOPQRSTVX")
 
@@ -29,21 +29,49 @@
 
 ;; Task 2
 
-(define H
-  (lambda (f g)
-    (lambda (m n)
-      (if (= n 0)
-          (f m)
-          (g m (H f (lambda (n) (- n 1))))
-          )
-    )))
+(define i
+ (lambda (m)
+   m))
+
+(define z
+  (lambda (m)
+    0
+    ))
+
+(define u
+  (lambda (m)
+    1
+    ))
 
 (define s2
   (lambda (u v)
     (+ 1 v)
     ))
 
-(define add (H (lambda (m) m) s2))
+(define H
+  (lambda (f g)
+    (lambda (m n)
+      (if (= n 0)
+          (f m)
+          (g m
+             ; (g m (add m (- n 1))
+             ; ((lambda (h) (h m (- n 1))) (H f g))
+             ((H f g) m (- n 1))
+             )
+          )
+      )))
+
+(define add (H i s2))
+(define mul (H z add))
+(define pow (H u mul))
 
 ; test
-(add 3 2)
+(add 0 0) ; 0
+(add 3 0) ; 3
+(add 1 5) ; 6
+(mul 0 5) ; 0
+(mul 1 5) ; 5
+(mul 5 4) ; 20
+(pow 2 0) ; 1
+(pow 2 1) ; 2
+(pow 2 5) ; 32
